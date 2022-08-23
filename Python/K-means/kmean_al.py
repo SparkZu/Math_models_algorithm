f6b2dd233a5ac7a531ec7d270eecd37dffb3e13d
ZuYong <10357213+sparkzu@user.noreply.gitee.com>
2022-08-23 20:58:00 +0800

    8-23-21

--- Python/K-means/kmean_al.py
+++ Python/K-means/kmean_al.py
@@ -1,9 +1,10 @@
 from sklearn import datasets
+from rich import print
 from sklearn.cluster import KMeans
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import scale
-import pandas as pd
+# import pandas as pd
 
 bc = datasets.load_breast_cancer()
 
