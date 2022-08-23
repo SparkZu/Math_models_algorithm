37e649f7334fd25c2bd2ed2817a47e46358154e6
ZuYong <10357213+sparkzu@user.noreply.gitee.com>
2022-08-23 23:30:02 +0800

    8-23-24

---
+++ Python/随机森林/ensenble_t.py
@@ -0,0 +1,15 @@
+from sklearn.datasets import load_breast_cancer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+bc = load_breast_cancer()
+print(bc.data.shape)
+param_grid = {'max_depth': np.arange(1, 20, 1)}
+rfc = RandomForestClassifier(n_estimators=39, random_state=90)
+GS = GridSearchCV(rfc, param_grid, cv=10)
+print(GS.fit(bc.data, bc.target))
+print(GS.best_params_)
